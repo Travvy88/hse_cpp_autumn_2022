8d210d8f5d1b545726731f742c97dbfe8291ae8f
travvy <shevtsov@ispras.com>
2022-12-01 14:31:44 +0300

    init

---
+++ 01/allocator.hpp
@@ -0,0 +1,14 @@
+class Allocator
+{
+public:
+    void makeAllocator(size_t maxSize){
+	
+
+};
+    char* alloc(size_t size) {
+
+};
+    void reset();
+    // Это деструктор!
+    ~Allocator();
+};
